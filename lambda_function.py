ad58e4eadf3b118389fc6f71b0539e0ed1699d51
Hernan <hd.martinezd1@uniandes.edu.co>
2024-03-16 01:09:50 -0500

    Updated Lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -34,8 +34,9 @@ def lambda_handler(event, context):
             TableName = "usuarios",
             Key = {"username": {"S": username}},
             UpdateExpression="set usuarios.total_request=:t",
-            ExpressionAttributeValues={":t": {'N':str(0)}}
-        )
+            ExpressionAttributeValues={":t": {'N':str(0)}},
+            ReturnValues="UPDATED_NEW",
+            )
 
         print('authorized')
         response = generatePolicy('user', 'Allow', event['methodArn'])
@@ -48,6 +49,7 @@ def lambda_handler(event, context):
             Key = {"username": {"S": username}},
             UpdateExpression="set usuarios.total_request=:t",
             ExpressionAttributeValues={":t": {'N':str(int(total_requests)+1)}},
+            ReturnValues="UPDATED_NEW",
         )
 
         print('unauthorized')
